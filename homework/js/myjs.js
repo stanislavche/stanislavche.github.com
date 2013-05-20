6ef79b9afeb47e233c7209c2bc1cbee78acd39f8
Stanislav <stress_tn@mail.ru>
2013-05-20 22:04:17 +0400

    fully adaptive without JS at all
    Fucking JS, it's so hard to anderstand for 2 lections))

---
+++ homework/js/myjs.js
@@ -0,0 +1,25 @@
+/*открытие, закрытие окна входа*/
+function hiddenLayer() {
+    document.getElementById("openning").style.visibility = "hidden";
+    document.getElementsByClassName('blur')[0].setAttribute('style', '-webkit-filter: none;');
+    return false;
+
+}
+function showLayer() {
+    document.getElementById("openning").style.visibility = "visible";
+    document.getElementsByClassName('blur')[0].setAttribute('style', '-webkit-filter: blur(2px);');
+    return false;
+}
+/*мягкое исчезновение*/
+$(document).ready(function(){
+    $("#head").show();
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() < 140) {
+                $('#head').fadeIn();
+            } else {
+                $('#head').fadeOut();
+            }
+        });
+    });
+});
