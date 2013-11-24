aeeb9453a7adab334ff947376fab2eb218a775b9
Stanislav <sc@cbwdt.com>
2013-11-24 17:23:51 +0400

    monitor breackable

--- piano/app/js/main.js
+++ piano/app/js/main.js
@@ -380,18 +380,31 @@ Piano.typewritter = function () {
 
 ;
 Piano.anim = function(){
-	var monitorGlich = true;
+	var n;
+	var $breaksound = new Audio('sounds/break.mp3');
+		$breaksound.preload = 'auto';
+		$breaksound.load();
 	var $glich = $('.monitor-glich');
-	myIntVal = setInterval(function(e) {
-		setTimeout(function () {
-			$glich.removeClass('active');
-		},50);
-		$glich.addClass('active');
-	}, 100);
+	n=0;
+	myIntVal = setInterval(Piano.glich, 100);
 	$(".monitor").click(function(){
+		n++;
 		clearInterval(myIntVal);
 		$glich.stop().removeClass('active');
+		$breaksound.currentTime=0;
+		$breaksound.play();
+		if(n==3){
+			n=0;
+			setInterval(Piano.glich, 100);
+		}
 	});
+};
+Piano.glich = function() {
+	var $glich = $('.monitor-glich');
+	setTimeout(function () {
+		$glich.removeClass('active');
+	},50);
+	$glich.addClass('active');
 };;
 $(document).load(function() {
 	$('.content-wrap').css("opacity","0");
