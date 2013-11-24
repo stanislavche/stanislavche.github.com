55b2e99758be7b9cc167644ee6f0536465e4ce0f
Stanislav <sc@cbwdt.com>
2013-11-24 15:47:03 +0400

    pianobuzz load

--- piano/app/js/main.js
+++ piano/app/js/main.js
@@ -396,12 +396,13 @@ Piano.anim = function(){
 $(".content-wrap").load(function() {
 	$('.content-wrap').css("opacity","0");
 	Piano.typewritter();
+	Piano.buzz();
 });
 $(document).ready(function() {
 	$('#loadscreen').fadeOut(300);
 	$('.content-wrap').css("opacity","1");
 	if (document.getElementById("monitor") !== null){
 		Piano.anim();
-		Piano.buzz();
+		
 	}
 });
--- piano/src/js/main.js
+++ piano/src/js/main.js
@@ -1,12 +1,13 @@
 $(".content-wrap").load(function() {
 	$('.content-wrap').css("opacity","0");
 	Piano.typewritter();
+	Piano.buzz();
 });
 $(document).ready(function() {
 	$('#loadscreen').fadeOut(300);
 	$('.content-wrap').css("opacity","1");
 	if (document.getElementById("monitor") !== null){
 		Piano.anim();
-		Piano.buzz();
+		
 	}
 });
