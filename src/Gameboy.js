2d49c321869d34642592006cbfff965ad1fd9f27
Stanislavche <stress_tn@yahoo.com>
2019-03-03 21:41:42 +0200

    screen add

--- src/Gameboy.js
+++ src/Gameboy.js
@@ -12,6 +12,7 @@ class Gameboy extends Component {
 	componentDidMount() {
 		let el = document.querySelector('#gameboy');
 		let aniContainer = document.querySelector('#animation');
+		let gameboyScreen = document.querySelector('#gameboy_screen');
 		let myAnimation = new LazyLinePainter(el, {
 			"strokeWidth" : 1.5,
 			"drawSequential": false,
@@ -23,6 +24,7 @@ class Gameboy extends Component {
 		myAnimation.on('complete', () => {
 			console.log('complete');
 			aniContainer.classList.remove("disactive");
+			gameboyScreen.classList.add("active");
 			setTimeout(() => {
 				ReactDOM.render(<App />, document.getElementById('root'));
 			}, 1500);
@@ -86,6 +88,7 @@ class Gameboy extends Component {
 					c1.4-0.7,3-0.3,4.4,0.2c0.6,0.2,1.1,0.4,1.5,0.9c0.8,1.1-0.3,2.6-1.4,3.4c-1.4,1-3,1.8-4.7,2.3"/>
 				<path className="st0" d="M56.9,269.2c0.5-2.8,2.7-5.3,5.5-5.7c2.8-0.4,5.9,1.8,5.9,4.6c0.1,3-2.8,5.2-5.6,6.3c-1.7,0.7-3.8,1-5.2-0.2
 					c-1.8-1.6-0.8-4.5,0.2-6.6c0.7-1.3,1.4-2.7,2.6-3.5s3.1-0.8,4,0.4"/>
+				<rect id="gameboy_screen" x="68" y="56" width="124" height="115"></rect>
 			</svg>
 		);
 	}
