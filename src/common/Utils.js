b344eba7a584cbb35640cfc2efc72fc14c824df6
Stanislavche <stress_tn@yahoo.com>
2019-03-03 22:25:31 +0200

    animated favicon

---
+++ src/common/Utils.js
@@ -0,0 +1,28 @@
+export function setAnimatedFavicon() {
+	let favicon_images = [
+			'./favicon/0.gif',
+			'./favicon/1.gif',
+			'./favicon/2.gif',
+			'./favicon/3.gif',
+			'./favicon/4.gif'
+		],
+		image_counter = 0,
+		head = document.head||document.getElementsByTagName("head")[0];
+
+	setInterval(function() {
+		document.getElementById('dynamic-favicon').remove();
+
+		let favikonEl = document.createElement('link');
+		favikonEl.setAttribute("id", "dynamic-favicon");
+		favikonEl.setAttribute("rel", "icon");
+		favikonEl.setAttribute("type", "image/gif");
+		favikonEl.setAttribute("href", favicon_images[image_counter]);
+		head.appendChild(favikonEl);
+		
+		if (image_counter === favicon_images.length -1) {
+			image_counter = 0;
+		} else {
+			image_counter++;
+		}
+	}, 200);
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Gameboy from './Gameboy';
 import * as serviceWorker from './serviceWorker';
+import * as Utils from './common/Utils';
+
+Utils.setAnimatedFavicon();
 
 ReactDOM.render(<Gameboy />, document.getElementById('animation'));
 
