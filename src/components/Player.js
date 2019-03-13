16656908b2298f979e31c472951a979869da5c3e
DIR\s.chernyavskiy <s.chernyavskiy@accenture.com>
2019-03-13 22:12:58 +0200

    Seo

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,7 +7,6 @@ class Announce extends Component {
 		let playlist = 'https://soundcloud.com/stress_tn/sets/playlist-for-website';
 		return (
 			<section className="container">
-				<h2 className="container__header">Player</h2>
 				<div className="container__wrapper player">
 					<ReactPlayer url={playlist} width="100%" height="500px" />
 				</div>
