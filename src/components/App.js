816d78a876622e6aeea14e08ef8bd90026906948
Stanislavche <stress_tn@yahoo.com>
2019-03-14 18:31:10 +0200

    404 error added and removed not necessary files

--- src/components/App.js
+++ src/components/App.js
@@ -7,34 +7,66 @@ import Game from './Game';
 import Donate from './Donate';
 import Events from './Events';
 import Player from './Player';
+import "./loadscreen.scss";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
 	render() {
-		return (
-			<div className="App">
-				<header className="App__header glitch">Stress_TN</header>
-				<div className="App__wrapper">
-					<div className="App__wrapper-cell">
-						<Bio />
-						<Player />
-						<Game />
+		let showIndexPage = () => {
+			return (
+				<div className="App">
+					<header className="App__header glitch">Stress_TN</header>
+					<div className="App__wrapper">
+						<div className="App__wrapper-cell">
+							<Bio />
+							<Cotacts />
+							<Game />
+						</div>
+						<div className="App__wrapper-cell">
+							<News />
+						</div>
+						<div className="App__wrapper-row">
+							<Discography />
+						</div>
+						<div className="App__wrapper-cell">
+							<Player />
+							<Donate />
+						</div>
+						<div className="App__wrapper-cell">
+							<Events />
+						</div>
 					</div>
-					<div className="App__wrapper-cell">
-						<News />
-					</div>
-					<div className="App__wrapper-row">
-						<Discography />
-					</div>
-					<div className="App__wrapper-cell">
-						<Cotacts />
-						<Donate />
-					</div>
-					<div className="App__wrapper-cell">
-						<Events />
-					</div>
+					<footer className="App__footer">All rights belong to Stress_TN. 2019</footer>
 				</div>
-				<footer className="App__footer">All rights belong to Stress_TN. 2019</footer>
-			</div>
+			);
+		};
+
+		let showErrorPage = () => {
+			return (
+				<div id="error" className="loadscreen errorLink">
+					<div className="loadscreen__c64-preview">
+						<p className="loadscreen__c64-preview_title">
+							**** PAGE NOT FOUND ****
+						<br />
+							ERROR 404
+						</p>
+						<p className="loadscreen__c64-preview_ram">READY.</p>
+						<p className="loadscreen__c64-preview_ram error-text"></p>
+						<p className="loadscreen__c64-preview_ram">FILE NOT FOUND</p>
+						<Link to="/" className="loadscreen__c64-preview_ram">GOTO MAIN PAGE</Link>
+					</div>
+			    </div>
+			);
+		};
+
+		return (
+			<Router>
+				<Switch>
+					<Route path="/" exact component={showIndexPage} />
+					<Route path="/error" component={showErrorPage} />
+					<Redirect from='*' to='/error' />
+				</Switch>
+			</Router>
 		);
 	}
 }
