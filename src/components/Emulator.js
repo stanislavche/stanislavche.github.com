8640bc532ac4fe4c2d4b110868ed0a1623897425
stanislavche <stress_tn@yahoo.com>
2020-06-09 01:44:54 +0300

    error but progress

--- src/components/Emulator.js
+++ src/components/Emulator.js
@@ -2,19 +2,67 @@ import React, { Component } from 'react';
 import Iframe from 'react-iframe'
 
 class Emulator extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			carts : [
+				{
+					fileName: '123',
+					image: '/images/extra/cart.png',
+					title: 'Bit dungeon I'
+				},{
+					fileName: 'mgb',
+					image: '/images/extra/cart.png',
+					title: 'Illusion'
+				}, {
+					fileName: '123',
+					image: '/images/extra/cart.png',
+					title: 'Absurd'
+				}
+			],
+			selectedCart: null
+		}
+	}
+
+	
 	render() {
+		let iframeBlock = () => {
+			if (!this.state.selectedCart) {
+				this.setState({selectedCart: this.state.carts[0]});
+			}
+
+			let fileName = "/gb/#" + this.state.selectedCart.fileName;
+
+			return (
+				<div>
+					<Iframe
+						url={fileName}
+						width="600px"
+						height="600px"
+						id="emulator"
+						className="myClassname"
+						display="initial"
+						position="relative" />
+				</div>
+			);
+		};
+
 		return (
 			<section className="container">
 				<h2 className="container__header">Select Cartridge</h2>
 				<div className="container__wrapper">
-					<Iframe
-						url="/gb/#123"
-				        width="600px"
-				        height="600px"
-				        id="emulator"
-				        className="myClassname"
-				        display="initial"
-				        position="relative"/>
+					<div className="container__row">
+						<div className="container__cell">
+							<ul className="cart__list">
+								{this.state.carts.map((item, key) =>
+									<div key={key}>{item.title}</div>
+								)}
+							</ul>
+						</div>
+						<div className="container__cell">
+							{ iframeBlock() }
+						</div>
+					</div>
 				</div>
 			</section>
 		);
