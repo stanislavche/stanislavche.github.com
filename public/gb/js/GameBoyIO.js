1e60dd497026e8f5aa20a5f56f38a8dca57e6d73
stanislavche <stress_tn@mail.ru>
2021-09-20 01:54:34 +0300

    fix audio issues

--- public/gb/js/GameBoyIO.js
+++ public/gb/js/GameBoyIO.js
@@ -9,11 +9,11 @@ var settings = [						//Some settings.
 	true,								//Colorize GB mode?
 	false,								//Disallow typed arrays?
 	8,									//Interval for the emulator loop.
-	10,									//Audio buffer minimum span amount over x interpreter iterations.
-	20,									//Audio buffer maximum span amount over x interpreter iterations.
+	40,									//Audio buffer minimum span amount over x interpreter iterations.
+	80,									//Audio buffer maximum span amount over x interpreter iterations.
 	false,								//Override to allow for MBC1 instead of ROM only (compatibility for broken 3rd-party cartridges).
 	false,								//Override MBC RAM disabling and always allow reading and writing to the banks.
-	false,								//Use the GameBoy boot ROM instead of the GameBoy Color boot ROM.
+	true,								//Use the GameBoy boot ROM instead of the GameBoy Color boot ROM.
 	false,								//Scale the canvas in JS, or let the browser scale the canvas?
 	true,								//Use image smoothing based scaling?
     [true, true, true, true]            //User controlled channel enables.
