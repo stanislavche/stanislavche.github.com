ba78892e1064caa9bec3e150f1cee5fbd7093a17
stanislavche <stress_tn@mail.ru>
2022-04-10 16:36:52 +0300

    clear description

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,7 +7,7 @@ class Bio extends Component {
 				<h2 className="container__header">Bio</h2>
 				<div className="container__wrapper">
 					<p className="container__text">Real name&nbsp;&mdash; Stanislav, chip-musician and video game music composer from Latvia.</p>
-					<p className="container__text">Since&nbsp;2008 Stanislav has started his own project&nbsp;&mdash; &laquo;S_TN&raquo;. Huge arsenal of&nbsp;musical gadgets provided new opportunities for the musician. M8 tracker, Game Boy, NES and Commodore turned into a&nbsp;weapon of&nbsp;mass destruction on&nbsp;the dance floor into his hand.</p>
+					<p className="container__text">Since&nbsp;2008 Stanislav has started his own project&nbsp;&mdash; &laquo;S_TN&raquo;. Huge arsenal of&nbsp;musical gadgets provided new opportunities for the musician. M8 tracker, Game Boy, NES and Commodore turned into a&nbsp;weapon of&nbsp;mass destruction on&nbsp;the dance floor.</p>
 				</div>
 			</section>
 		);
