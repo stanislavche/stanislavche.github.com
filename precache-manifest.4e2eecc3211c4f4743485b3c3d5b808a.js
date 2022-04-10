62e605199cd87fc6bc5848b480d6d9736906a9bc
stanislavche <stress_tn@mail.ru>
2022-04-10 17:18:52 +0300

    Updates

--- precache-manifest.4e2eecc3211c4f4743485b3c3d5b808a.js
+++ precache-manifest.4e2eecc3211c4f4743485b3c3d5b808a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8ea4a55170abb163344a",
-    "url": "/static/css/main.17337742.chunk.css"
+    "revision": "cfe30b3e3ec005442f5b",
+    "url": "/static/css/main.9041ed11.chunk.css"
   },
   {
-    "revision": "8ea4a55170abb163344a",
-    "url": "/static/js/main.8ea4a551.chunk.js"
+    "revision": "cfe30b3e3ec005442f5b",
+    "url": "/static/js/main.cfe30b3e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -20,20 +20,20 @@ self.__precacheManifest = [
     "url": "/static/media/tv.a5114486.png"
   },
   {
-    "revision": "1efe351f32cd00b8c050a30ebf8d0fb9",
-    "url": "/static/media/C64_User_v1.0-STYLE.1efe351f.woff"
-  },
-  {
     "revision": "6c16d1b1b41eb401e2045374fc8f2b18",
     "url": "/static/media/C64_User_v1.0-STYLE.6c16d1b1.eot"
   },
   {
-    "revision": "b5c0c4ab27b9b0b4cb53e347a3addbe8",
-    "url": "/static/media/C64_User_Mono_v1.0-STYLE.b5c0c4ab.eot"
+    "revision": "1efe351f32cd00b8c050a30ebf8d0fb9",
+    "url": "/static/media/C64_User_v1.0-STYLE.1efe351f.woff"
   },
   {
     "revision": "927533f2407c1671b4332c31ebe69c02",
     "url": "/static/media/C64_User_v1.0-STYLE.927533f2.ttf"
+  },
+  {
+    "revision": "b5c0c4ab27b9b0b4cb53e347a3addbe8",
+    "url": "/static/media/C64_User_Mono_v1.0-STYLE.b5c0c4ab.eot"
   },
   {
     "revision": "c98949fa9320075f4e4f8c4c45e96139",
@@ -44,28 +44,28 @@ self.__precacheManifest = [
     "url": "/static/media/C64_User_Mono_v1.0-STYLE.5cd08674.ttf"
   },
   {
-    "revision": "038642c5bf2240322e77868641d21edb",
-    "url": "/static/media/C64_Pro_v1.0-STYLE.038642c5.woff"
-  },
-  {
     "revision": "25c5706f165b605581d25e290f41ca8a",
     "url": "/static/media/C64_Pro_v1.0-STYLE.25c5706f.eot"
   },
   {
-    "revision": "8ebc530c480b4361a5e7744fa87db927",
-    "url": "/static/media/C64_Pro_Mono_v1.0-STYLE.8ebc530c.eot"
+    "revision": "038642c5bf2240322e77868641d21edb",
+    "url": "/static/media/C64_Pro_v1.0-STYLE.038642c5.woff"
   },
   {
     "revision": "652c538faf1267e6b8c23970b859a0ea",
     "url": "/static/media/C64_Pro_v1.0-STYLE.652c538f.ttf"
   },
   {
-    "revision": "ae4fe1f14285762ebfbaf0ee03ef0c79",
-    "url": "/static/media/C64_Pro_Mono_v1.0-STYLE.ae4fe1f1.ttf"
+    "revision": "8ebc530c480b4361a5e7744fa87db927",
+    "url": "/static/media/C64_Pro_Mono_v1.0-STYLE.8ebc530c.eot"
   },
   {
     "revision": "fb430063d59915d7222aaf13ae7809b3",
     "url": "/static/media/C64_Pro_Mono_v1.0-STYLE.fb430063.woff"
+  },
+  {
+    "revision": "ae4fe1f14285762ebfbaf0ee03ef0c79",
+    "url": "/static/media/C64_Pro_Mono_v1.0-STYLE.ae4fe1f1.ttf"
   },
   {
     "revision": "5bd9a29db30b2419f93e074124f2404d",
@@ -112,7 +112,7 @@ self.__precacheManifest = [
     "url": "/static/media/gameboy.97f4230a.svg"
   },
   {
-    "revision": "31b5605bc7a629d82d06651be22b00c2",
+    "revision": "08f05644f759cac6815dc5eb735e19c9",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e903530ebec61b8b18ddbc9ab7a59b16.js"
+  "/precache-manifest.4e2eecc3211c4f4743485b3c3d5b808a.js"
 );
 
 workbox.clientsClaim();
