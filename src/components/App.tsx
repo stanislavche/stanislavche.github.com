b07a39480a5079e3d20bee40ec9f9619f50ce910
Stanislav Chernyavskiy <schernyavskiy@iikosoftware.com>
2025-08-01 20:04:08 +0300

    Set visibility of page data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import Bio from './Bio';
 import News from './News';
 import Discography from './Discography';
 import Cotacts from './Cotacts';
 import Game from './Game';
 import Emulator from './Emulator';
-//import Donate from './Donate';
+import Donate from './Donate';
 import Events from './Events';
 import Player from './Player';
 import Kits from './Kits';
 import "./loadscreen.scss";
 import { BrowserRouter as Router, Route, Link, Routes, Navigate } from "react-router-dom";
 import Mup from "./Mup";
+import { AppContext } from '../context/AppContext';
 
 class App extends Component {
+	static contextType = AppContext;
+
+	state = {
+		visibleSections: null
+	};
+
+	componentDidMount() {
+		const ctx = this.context;
+		if (ctx?.visibleSections) {
+			this.setState({ visibleSections: ctx.visibleSections });
+		}
+	}
+
 	render() {
 		const ShowIndexPage = () => {
+			const visible = this.state.visibleSections || {};
 			return (
 				<div className="App">
 					<header className="App__header glitch">
@@ -24,26 +39,32 @@ class App extends Component {
 						{/*<span className="App__hand-rotate" role="img" aria-labelledby="peace-2">✌</span>*/}
 					</header>
 					<div className="App__wrapper">
+						{visible.bio && (
+							<div className="App__wrapper-cell">
+								<Bio />
+							</div>
+						)}
+						{visible.news && (
+							<div className="App__wrapper-cell">
+								<News />
+							</div>
+						)}
+						{visible.discography && (
+							<div className="App__wrapper-row">
+								<Discography />
+							</div>
+						)}
 						<div className="App__wrapper-cell">
-							<Bio />
+							{visible.game && <Game />}
+							{visible.player && <Player />}
+							{visible.events && <Events />}
+							{visible.donate && <Donate />}
+							{visible.contacts && <Cotacts />}
 						</div>
 						<div className="App__wrapper-cell">
-							<News />
-						</div>
-						<div className="App__wrapper-row">
-							<Discography  />
-						</div>
-						<div className="App__wrapper-cell">
-							<Game />
-							<Player />
-							<Events />
-							{/*<Donate />*/}
-						</div>
-						<div className="App__wrapper-cell">
-							{/*<Mup />*/}
-							<Emulator />
-							<Kits />
-							<Cotacts />
+							{visible.mup && <Mup />}
+							{visible.emulator && <Emulator />}
+							{visible.kits && <Kits />}
 						</div>
 					</div>
 					<footer className="App__footer">All rights belong to S_TN</footer>
